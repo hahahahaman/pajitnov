b9c1dab2c2f25e13dd78bc8cec0f1f5ea1a79145
Ed Ye <hahahadude@gmail.com>
2015-11-04 05:13:16 -0500

    added some restart code

--- src/pajitnov.lisp
+++ src/pajitnov.lisp
@@ -4,20 +4,40 @@
 
 ;;; "pajitnov" goes here. Hacks and glory await!
 
-
 (defun initialize ())
 (defun handle-input ()
   (when (key-action-p :escape :press)
-    (cl-glfw3:set-window-should-close)))
+    (close-window))
+  (when (and (key-pressed-p :left-control)
+             (key-pressed-p :left-alt)
+             (key-action-p :r :press))
+    (set-restart-window)
+    (close-window)))
+
 (defun render ())
-(defun update ())
+(defun update ()
+  )
 (defun cleanup ()
   t)
 
+(defmacro start-window ()
+  `(err:run "pajitnov"
+            :init-code (initialize)
+            :input-code (handle-input)
+            :render-code (render)
+            :update-code (update)
+            :cleanup-code (cleanup)))
+
+(let ((restart nil))
+  (defun set-restart-window (&optional (value t))
+    (setf restart value))
+  (defun restart-window? ()
+    restart)
+  (defun restart-window ()
+    (setf restart nil)
+    (start-window)))
+
 (defun game ()
-  (err:run "insert-title"
-           :init-code (initialize)
-           :input-code (handle-input)
-           :render-code (render)
-           :update-code (update)
-           :cleanup-code (cleanup)))
+  (start-window)
+  (iter (while (restart-window?))
+    (restart-window)))
