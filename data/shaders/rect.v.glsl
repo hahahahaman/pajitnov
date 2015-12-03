8ad7ae68dce83b174691a6053cabf3081241ba20
Ed Ye <hahahadude@gmail.com>
2015-12-03 03:12:48 -0500

    added view matrix to the rect and sprite shaders

--- data/shaders/rect.v.glsl
+++ data/shaders/rect.v.glsl
@@ -4,9 +4,10 @@ layout (location = 0) in vec2 vertex;
 out vec2 TexCoords;
 
 uniform mat4 model;
+uniform mat4 view;
 uniform mat4 projection;
 
 void main()
 {
-  gl_Position = projection * model * vec4(vertex, 0.0f, 1.0f);
+  gl_Position = projection * view * model * vec4(vertex, 0.0f, 1.0f);
 }
